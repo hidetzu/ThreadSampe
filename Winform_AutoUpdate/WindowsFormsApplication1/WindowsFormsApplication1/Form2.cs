003cb6f33e4174017d25e5dde7c4be34a36c6a83
hidetzu <hidetzu@gmail.com>
2016-06-27 21:48:35 +0900

    スレッドの再開、停止

---
+++ Winform_AutoUpdate/WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        private Thread _workerThread;
+        ManualResetEvent _event = new ManualResetEvent(true);
+
+
+        public Form2()
+        {
+            InitializeComponent();
+            _workerThread = new Thread(
+                () =>
+                {
+                    long count = 0;
+                    while (true)
+                    {
+                        _event.WaitOne();
+                        Thread.Sleep(1000);
+                        Invoke(
+                            (MethodInvoker)
+                            delegate ()
+                            {
+                                label1.Text = string.Format("{0}", count++);
+                                label2.Text = string.Format("{0}", count++);
+                                label3.Text = string.Format("{0}", count++);
+                            });
+                    }
+                });
+
+            _workerThread.Start();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _workerThread.Abort();
+            _workerThread.Join();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _event.Reset();
+            MessageBox.Show("AAAAA");
+            _event.Set();
+        }
+    }
+}
